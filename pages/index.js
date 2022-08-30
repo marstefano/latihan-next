207c93c643b920b8d519fdb06cb0602ee2115717
marstefano <stefano.2020@smk-strada.sch.id>
2022-08-30 11:28:29 +0700

    latihan

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Avatar  from '@mui/material/Avatar'
 
 export default function Home() {
   return (
@@ -15,7 +16,7 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-
+        <Avatar>S</Avatar>
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
